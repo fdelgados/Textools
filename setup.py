caadd0e97fa9aac10d1dbc29227b144d93f0f7ce
Cisco Delgado <fdelgados@gmail.com>
2020-02-22 13:44:08 +0100

    Upgrade version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="texcptulz",
     packages=['txtools',],
-    version="2.0.1",
+    version="2.5.0",
     author="Cisco Delgado",
     author_email="fdelgados@gmail.com",
     description="Tools for cleaning and preprocessing text",
